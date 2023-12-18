337a674a448a2f5fcf75e2cbe397b9a19afc8bec
emohamedd <a.elgaouej@gmail.com>
2023-12-18 20:33:54 +0100

    finishing ex00

---
+++ ex00/main.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 18:30:34 by emohamed          #+#    #+#             */
+/*   Updated: 2023/12/18 18:46:06 by emohamed         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+template <typename T> 
+void swap(T& x, T& y);
+template <typename T> 
+T min(T x, T y);
+template <typename T> 
+T max (T x, T y);
